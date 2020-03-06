b1db41d305f73a8080f6249bfdd1233b823298e9
sidhant <sidhantdas@gmail.com>
2020-03-06 09:01:38 +0530

    changes

--- src/main/scala/Hardware/TemperaturePowerShell.scala
+++ src/main/scala/Hardware/TemperaturePowerShell.scala
@@ -14,14 +14,13 @@ object TemperaturePowerShell extends App {
 
 
     val temperatureResponse = pwrShl.executeCommand("Get-WmiObject -Class Win32_PerfFormattedData_Counters_ThermalZoneInformation |%{ \"{0,-10}{1,-1}\" -f $_.Name,$_.Temperature}").getCommandOutput
-    println(s"${temperatureResponse}")
     temperatureResponse.split("\n").foreach {
       ln =>
         val arr = ln.split(" ")
         val sensorName = s"${arr(0)}  ${LocalDateTime.now()}"
         val temperature = arr(1)
-        println(sensorName)
-        println(temperature)
+        //println(sensorName)
+        //println(temperature)
         val senseData = SensorData(sensorName, temperature)
 
         kfkProducer.addRecod(senseData)
